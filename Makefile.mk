9f640d7724e6fc1a4e6e0f53fb305ab47983f2f8
Pratyush Sharma <41389786+PratyushSharma314@users.noreply.github.com>
2020-09-13 00:28:23 +0530

    Deleted Makefile.mk

--- Makefile.mk
+++
@@ -1,9 +0,0 @@
-NAME=udemy-ml-3
-#COMMIT_ID=$(shell git rev-parse HEAD)
-
-
-build-ml-api-heroku:
-	docker build --build-arg PIP_EXTRA_INDEX_URL=${PIP_EXTRA_INDEX_URL} -t registry.heroku.com/$(NAME)/web .
-
-push-ml-api-heroku:
-	docker push registry.heroku.com/${HEROKU_APP_NAME}/web:latest
